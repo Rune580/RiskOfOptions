2018d88fa0c49f318ade44d295415d4df7d77256
Rune <RuneMasterGaming@users.noreply.github.com>
2020-09-22 13:51:44 -0400

    Used BepinEX Template
    yeah

---
+++ RiskOfOptionsPlugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+
+/*
+using R2API;
+using R2API.Utils;
+*/
+
+
+/// <summary>
+/// Things to do: 
+///     Make sure your references are located in a "libs" folder that's sitting next to the project folder.
+///         This folder structure was chosen as it was noticed to be one of the more common structures.
+///     Add a NuGet Reference to Mono.Cecil. The one included in bepinexpack3.0.0 on thunderstore is the wrong version 0.10.4. You want 0.11.1.
+///         You can do this by right clicking your project (not your solution) and going to "Manage NuGet Packages".
+///    Make sure the AUTHOR field is correct.
+///    Make sure the MODNAME field is correct.
+///    Delete this comment!
+///    Oh and actually write some stuff.
+/// </summary>
+
+
+
+namespace RiskOfOptions
+{
+
+    //[BepInDependency("com.bepis.r2api")]
+    //[R2APISubmoduleDependency(nameof(yourDesiredAPI))]
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public sealed class RiskOfOptionsPlugin : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "RiskOfOptions",
+            AUTHOR = "rune580",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0a";
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Awake is automatically called by Unity")]
+        private void Awake() //Called when loaded by BepInEx.
+        {
+
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Start is automatically called by Unity")]
+        private void Start() //Called at the first frame of the game.
+        {
+
+        }
+    }
+}
