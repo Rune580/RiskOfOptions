8bdfda4248bae3673ed738dc5ebd8a785e2b895b
Rune <RuneMasterGaming@users.noreply.github.com>
2020-09-23 18:50:44 -0400

    adding images, clearing leftover dumb stuff

--- RiskOfOptionsPlugin.cs
+++ RiskOfOptionsPlugin.cs
@@ -19,34 +19,6 @@ namespace RiskOfOptions
         private void Awake()
         {
             ModSettingsManager.Init();
-
-            ModSettingsManager.setPanelDescription("Testing stuff");
-
-            ModSettingsManager.setPanelTitle("Risk of Options Testing Stuff");
-
-            ModSettingsManager.addOption(new ModOption(ModOption.OptionType.Slider, "Test Slider", "This is a Slider test."));
-
-            ModSettingsManager.addListener(ModSettingsManager.getOption("Test Slider"), new UnityEngine.Events.UnityAction<float>(testerino));
-
-            ModSettingsManager.addOption(new ModOption(ModOption.OptionType.Bool, "Test Bool", "This is a Bool test."));
-
-            ModSettingsManager.addListener(ModSettingsManager.getOption("Test Bool"), new UnityEngine.Events.UnityAction<bool>(test2));
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Start is automatically called by Unity")]
-        private void Start() //Called at the first frame of the game.
-        {
-            
-        }
-
-        public void testerino(float dum)
-        {
-            //Debug.Log($"{dum}");
-        }
-
-        public void test2(bool free)
-        {
-            //Debug.Log($"{free}");
         }
     }
 }
