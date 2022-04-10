5164b02da0c14589a675ee3d1d734a7e86a141da
Rune <12521934+Rune580@users.noreply.github.com>
2022-04-09 22:11:24 -0400

    2.3.1

--- RiskOfOptionsPlugin.cs
+++ RiskOfOptionsPlugin.cs
@@ -10,7 +10,7 @@ namespace RiskOfOptions
             ModName = "Risk Of Options",
             Author = "rune580",
             Guid = "com." + Author + "." + "riskofoptions",
-            Version = "2.3.0";
+            Version = "2.3.1";
 
         internal static ConfigEntry<bool> seenNoMods;
         internal static ConfigEntry<bool> seenMods;
