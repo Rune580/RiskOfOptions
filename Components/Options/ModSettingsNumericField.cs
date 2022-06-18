489fc26704086c0d113194f2ea4177c732df98ba
Rune580 <12521934+Rune580@users.noreply.github.com>
2022-06-18 11:10:26 -0400

    Laying ground work for numeric input fields

---
+++ Components/Options/ModSettingsNumericField.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+
+namespace RiskOfOptions.Components.Options;
+
+public class ModSettingsNumericField<TNumeric> : ModSettingsControl<TNumeric>
+    where TNumeric : struct, IComparable, IComparable<TNumeric>, IConvertible, IEquatable<TNumeric>, IFormattable
+{
+    public TMP_InputField valueText;
+    public TNumeric min;
+    public TNumeric max;
+    public string formatString;
+
+    protected override void Disable()
+    {
+        
+    }
+
+    protected override void Enable()
+    {
+        
+    }
+}
