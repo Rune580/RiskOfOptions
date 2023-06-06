85091de4b93aa47cff99fe0804a36eade1d42ff0
Rune580 <runemaster580@gmail.com>
2023-06-06 10:43:23 -0400

    bump version to 2.6.0

--- RiskOfOptions/RiskOfOptionsPlugin.cs
+++ RiskOfOptions/RiskOfOptionsPlugin.cs
@@ -17,7 +17,7 @@ namespace RiskOfOptions
             ModName = "Risk Of Options",
             Author = "rune580",
             Guid = "com." + Author + "." + "riskofoptions",
-            Version = "2.5.3";
+            Version = "2.6.0";
 
         internal static ConfigEntry<bool> seenNoMods;
         internal static ConfigEntry<bool> seenMods;
