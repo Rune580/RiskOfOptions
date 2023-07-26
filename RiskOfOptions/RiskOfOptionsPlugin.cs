6ba4f4fe9bd862156cd9131d688e97fa3039ec8b
Rune580 <runemaster580@gmail.com>
2023-07-26 12:08:50 -0400

    namespace moment

--- RiskOfOptions/RiskOfOptionsPlugin.cs
+++ RiskOfOptions/RiskOfOptionsPlugin.cs
@@ -8,27 +8,26 @@ using BepInEx.Configuration;
 #pragma warning restore CS0618 // Type or member is obsolete
 [module: UnverifiableCode]
 
-namespace RiskOfOptions
+namespace RiskOfOptions;
+
+[BepInPlugin(Guid, ModName, Version)]
+public sealed class RiskOfOptionsPlugin : BaseUnityPlugin
 {
-    [BepInPlugin(Guid, ModName, Version)]
-    public sealed class RiskOfOptionsPlugin : BaseUnityPlugin
+    private const string
+        ModName = "Risk Of Options",
+        Author = "rune580",
+        Guid = "com." + Author + "." + "riskofoptions",
+        Version = "2.6.1";
+
+    internal static ConfigEntry<bool> seenNoMods;
+    internal static ConfigEntry<bool> seenMods;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Awake is automatically called by Unity")]
+    private void Awake()
     {
-        private const string
-            ModName = "Risk Of Options",
-            Author = "rune580",
-            Guid = "com." + Author + "." + "riskofoptions",
-            Version = "2.6.1";
-
-        internal static ConfigEntry<bool> seenNoMods;
-        internal static ConfigEntry<bool> seenMods;
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Awake is automatically called by Unity")]
-        private void Awake()
-        {
-            seenNoMods = Config.Bind("One Time Stuff", "Has seen the no mods prompt", false);
-            seenMods = Config.Bind("One Time Stuff", "Has seen the mods prompt", false);
+        seenNoMods = Config.Bind("One Time Stuff", "Has seen the no mods prompt", false);
+        seenMods = Config.Bind("One Time Stuff", "Has seen the mods prompt", false);
             
-            ModSettingsManager.Init();
-        }
+        ModSettingsManager.Init();
     }
 }
