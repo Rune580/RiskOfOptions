cce5e7ab5a4a29ba7122213db5358383b9e2119c
Rune580 <runemaster580@gmail.com>
2026-08-28 16:35:56 -0400

    removed commented out code

--- RiskOfOptions/Components/RuntimePrefabs/KeyBindPrefab.cs
+++ RiskOfOptions/Components/RuntimePrefabs/KeyBindPrefab.cs
@@ -11,27 +11,7 @@ namespace RiskOfOptions.Components.RuntimePrefabs
         {
             KeyBind = Object.Instantiate(Prefabs.keyBindButton);
         }
-
-        // public void Instantiate(GameObject settingsPanel)
-        // {
-        //     GameObject gameObject = settingsPanel.transform.Find("SafeArea").Find("SubPanelArea").Find("SettingsSubPanel, Controls (M&KB)")
-        //         .Find("Scroll View").Find("Viewport").Find("VerticalLayout").Find("SettingsEntryButton, Binding (Jump)").gameObject;
-        //
-        //     KeyBind = Object.Instantiate(gameObject);
-        //     KeyBind.name = "Mod Option Prefab, KeyBind";
-        //     
-        //     var inputBindingControl = KeyBind.GetComponentInChildren<InputBindingControl>();
-        //     var controller = KeyBind.AddComponent<ModSettingsKeyBind>();
-        //
-        //     controller.nameLabel = inputBindingControl.nameLabel;
-        //     
-        //     Object.DestroyImmediate(inputBindingControl);
-        //     Object.DestroyImmediate(KeyBind.GetComponentInChildren<InputBindingDisplayController>());
-        //
-        //     foreach (var button in KeyBind.GetComponentsInChildren<HGButton>())
-        //         button.onClick.RemoveAllListeners();
-        // }
-
+        
         public void Destroy()
         {
             Object.DestroyImmediate(KeyBind);
